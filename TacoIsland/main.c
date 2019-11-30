f31d3a68add44e1b5376d8a17532165c0c7f142c
= <jmb@eeepc>
2019-11-29 21:59:09 -0500

    added a thread to generate the bitmap; so far it works

--- TacoIsland/main.c
+++ TacoIsland/main.c
@@ -5,15 +5,22 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <pthread.h>
+#include <unistd.h>
 
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
-#define SCREEN_FPS 23.976
-#define TEX_FPS 5
+#define SCREEN_FPS 60
+#define TEX_FPS 60
 //23.976
 
 GLuint tex   = 0;
+GLuint* bitmap = NULL;
+pthread_t updCb;
+pthread_mutex_t bitmapMutex;
 bool texLock = false;
+bool stillAlive = true;
+
 //GLuint tex_w = 0;
 //GLuint tex_h = 0;
 
@@ -34,12 +41,11 @@ bool initGL() {
 }
 
 GLuint* getCheckerboard(GLubyte r1, GLubyte g1, GLubyte b1, GLubyte a1,
-	      GLubyte r2, GLubyte g2, GLubyte b2, GLubyte a2) {
+			GLubyte r2, GLubyte g2, GLubyte b2, GLubyte a2) {
 	GLuint* retval = NULL;
 	const int cbWidth = 128;
 	const int cbHeight = 128;
 	const int cbPixels = cbWidth * cbHeight;
-
 	retval = malloc(sizeof(GLuint)*cbPixels);
 	for(int i=0; i<cbPixels; ++i) {
 		GLubyte* pixel = (GLubyte*)&retval[i];
@@ -55,19 +61,19 @@ GLuint* getCheckerboard(GLubyte r1, GLubyte g1, GLubyte b1, GLubyte a1,
 			pixel[3] = a2;
 		}
 	}
-
 	return retval;
 }
 
-GLuint getCBTexture(GLubyte r1, GLubyte g1, GLubyte b1, GLubyte a1,
-	      GLubyte r2, GLubyte g2, GLubyte b2, GLubyte a2) {
+//GLuint getCBTexture(GLubyte r1, GLubyte g1, GLubyte b1, GLubyte a1,
+//	      GLubyte r2, GLubyte g2, GLubyte b2, GLubyte a2) {
+GLuint getCBTexture() {
 	GLuint retval = 0;
-	GLuint* cb = getCheckerboard(r1, g1, b1, a1, r2, g2, b2, a2);
+	GLuint* cb = bitmap; //getCheckerboard(r1, g1, b1, a1, r2, g2, b2, a2);
 	assert(cb != NULL);
 	glGenTextures(1, &retval);
 	glBindTexture(GL_TEXTURE_2D, retval);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 128, 128, 0, GL_RGBA, GL_UNSIGNED_BYTE, cb);
-	free(cb);	//iffy part 1
+	//free(cb);	//iffy part 1
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
@@ -84,12 +90,14 @@ GLuint getCBTexture(GLubyte r1, GLubyte g1, GLubyte b1, GLubyte a1,
 
 void update() {
 	if(tex != 0) glDeleteTextures(1, &tex);
-	tex = getCBTexture(
+/*	tex = getCBTexture(
 //			(GLuint)(rand()%255), (GLuint)(rand()%255), (GLuint)(rand()%255), (GLuint)(rand()%255),
 			(GLuint)(rand()%255), (GLuint)(rand()%255), (GLuint)(rand()%255), (GLuint)(255),
 //			(GLuint)(63+rand()%127), (GLuint)(63+rand()%31), (GLuint)(63+rand()%31), (GLuint)(255),
 			(GLuint)(31+rand()%127), (GLuint)(31+rand()%127), (GLuint)(31+rand()%127), (GLuint)(255));
 //			0,0,0,0);
+*/
+	tex = getCBTexture();
 }
 
 void render() {
@@ -114,20 +122,35 @@ void render() {
 }
 
 void runMainLoop(int val) {
-//	update();
 	while(texLock);  // wait until tex is unlocked
 	texLock = true;
+	pthread_mutex_lock(&bitmapMutex);
+	update();
+	pthread_mutex_unlock(&bitmapMutex);
 	render();
 	texLock = false;
 	glutTimerFunc(1000/SCREEN_FPS, runMainLoop, val);
 }
 
+/*
 void runUpdateLoop(int val) {
 	while(texLock);
 	texLock = true;
 	update();
 	texLock = false;
 	glutTimerFunc(1000/TEX_FPS, runUpdateLoop, val);
+}
+*/
+
+void* updateCheckerboard(void* arg) {
+	while(stillAlive) {
+		pthread_mutex_lock(&bitmapMutex);
+		if(bitmap != NULL) free(bitmap);
+		bitmap = getCheckerboard(0xFF,0xFF,0xFF,0xFF, rand()%255,0x00,0x00,0xFF);
+		pthread_mutex_unlock(&bitmapMutex);
+		usleep(1000/TEX_FPS*1000);
+	}
+	pthread_exit(NULL);
 }
 
 int main(int argc, char** argv) {
@@ -141,15 +164,21 @@ int main(int argc, char** argv) {
 		return -1;
 	}
 
+	pthread_mutex_init(&bitmapMutex, NULL);
+	bitmap = getCheckerboard(0xFF, 0x7F, 0x00, 0xFF, 0xFF, 0x3F, 0x00, 0xFF);
 	//tex = getCBTexture(0xFF, 0x7F, 0x00, 0xFF, 0xFF, 0x3F, 0x00, 0xFF);
 	
 	//glutKeyboardFunction(handleKeys);
 	glutDisplayFunc(render);
 
 	glutTimerFunc(1000/SCREEN_FPS, runMainLoop, 0);
-	glutTimerFunc(1000/TEX_FPS, runUpdateLoop, 0);
+//	glutTimerFunc(1000/TEX_FPS, runUpdateLoop, 0);
+	pthread_create(&updCb, NULL, updateCheckerboard, NULL);
 	glutMainLoop();
+	stillAlive = false;
 
 	if(tex != 0) glDeleteTextures(1, &tex);
+	pthread_mutex_destroy(&bitmapMutex);
+	pthread_exit(NULL);
 	return 0;
 }
